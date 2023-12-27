31f603a8e066933d2ea09339d1d68895914436be
kyongseo <102573192+kyounggseo@users.noreply.github.com>
2023-12-27 15:46:11 +0900

    feat: 오늘 지출 추천 기능 스케줄러 추가 #13

---
+++ src/main/java/com/wanted/budgetmanagement/BudgetManagementApplication.java
@@ -0,0 +1,15 @@
+package com.wanted.budgetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class BudgetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementApplication.class, args);
+	}
+
+}
